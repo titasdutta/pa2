57eb78c1d215eabd711e3830407e3b9d21389ec7
Titas Dutta <titasdutta@users.noreply.github.com>
2022-04-30 11:28:00 -0700

    Added Servlets for Product and ProductList

---
+++ src/main/java/com/example/disc3/ProductServlet.java
@@ -0,0 +1,27 @@
+package com.example.disc3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "product", value = "/product")
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
