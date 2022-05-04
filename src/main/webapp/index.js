f08b1807f4f31ee1da5679f0d57800bbb6710866
Titas Dutta <titasdutta@users.noreply.github.com>
2022-05-03 18:38:00 -0700

    Updated index.js

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,1 +1,43 @@
-document.getElementById("")
+$(document).ready(function() {
+    jQuery.ajax({
+            url: "./product_list",
+            method: "GET",
+            success: function(result){
+                popProducts(result);
+            },
+            error: function(result){
+                console.log("Something went wrong!");
+            }
+        });
+});
+
+function popProducts(result){
+    console.log(result);
+    let table = $("#store_items");
+    let table_row = "";
+    table_row = generateImageRow(table_row,result,0);
+    table_row = generateDescRow(table_row,result,0);
+    table_row = generateImageRow(table_row,result,5);
+    table_row = generateDescRow(table_row,result,5);
+    table.append(table_row);
+}
+
+function generateImageRow(table_row, result, start){
+    table_row = table_row + "<tr>";
+    for(let i = start; i < start+5; i++){
+        table_row = table_row + "<td><a href=\"./detailed_description.html?name=" + result[i]["product"] + "\">"
+        + "<img src=\"" + result[i]["image"] + "\">" + "</a></td>";
+    }
+    table_row = table_row + "</tr>";
+    return table_row;
+}
+
+function generateDescRow(table_row, result, start){
+    table_row = table_row + "<tr>";
+    for(let i = start; i < start+5; i++){
+        table_row = table_row + "<td><a href=\"./detailed_description.html?name=" + result[i]["product"] + "\" id=\"" + result[i]["product"]
+        + "\">" + result[i]["product"] + "</a>" + "<br> $" + result[i]["price"] + "</td>";
+    }
+    table_row = table_row + "</tr>";
+    return table_row;
+}
