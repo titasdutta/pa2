a766a9f3fd1db3bcad00631e8450341e410332ee
Titas Dutta <titasdutta@users.noreply.github.com>
2022-05-04 09:43:02 -0700

    Added remaining data validations for form

--- src/main/webapp/order_form.js
+++ src/main/webapp/order_form.js
@@ -53,7 +53,58 @@ function checkName(messages){
 }
 
 function checkZip(messages){
+    var zip = document.getElementById("zip").value;
+    var digits = /^[0-9]+$/;
+    if(!zip){
+        messages.push("Please enter a zip code!");
+    } else if(!zip.match(digits)) {
+        messages.push("Zip code should only contain digits 0-9!");
+    } else if(zip.length > 5){
+        messages.push("Zip code should be at max 5 digits!");
+    }
+}
 
+function checkPhone(messages){
+    var phone = document.getElementById("phone").value;
+    var phone_format = /^\d{10}$/;
+
+    if(!phone){
+        messages.push("Please enter a phone number!");
+    } else if(!phone.match(phone_format)){
+        messages.push("Phone number must be valid format!");
+    } else if(phone.length > 10){
+        messages.push("Phone number should be at max 10 digits!");
+    }
+}
+
+function checkEmail(messages){
+    var email = document.getElementById("email").value;
+    var email_format = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    if(!email){
+        messages.push("Please enter an email!");
+    } else if(!email.match(email_format)){
+        messages.push("Email must be valid format!");
+    }
+}
+
+function checkCredit(messages){
+    var credit_card = document.getElementById("ccn").value;
+    var cvv = document.getElementById("cvv").value;
+    var expiry = document.getElementById("expiry").value;
+    var digits = /^[0-9]+$/;
+
+    if(!credit_card){
+        messages.push("Please enter a credit card number!");
+    } else if(!credit_card.match(digits)) {
+        messages.push("Credit card number should only contain digits 0-9!");
+    }
+
+    if(!cvv){
+        messages.push("Please enter a CVV!");
+    } else if(!cvv.match(digits)) {
+        messages.push("CVV should only contain digits 0-9!");
+    }
 }
 
 function printErrorMessages(messages){
@@ -70,6 +121,10 @@ function printErrorMessages(messages){
 document.getElementById("place_order").onclick = function() {
     var messages = [];
     checkName(messages);
+    checkZip(messages);
+    checkPhone(messages);
+    checkEmail(messages);
+    checkCredit(messages);
      if(messages.length > 0){
         printErrorMessages(messages);
      } else {
